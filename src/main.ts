e5fca4e09e03752a58c609df239eecdb16b97f99
Eric Stolten <eric.stolten@adp.com>
2016-05-04 18:02:23 -0400

    Add HTTP_PROVIDERS to bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+
 import {MainAppComponent, environment} from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(MainAppComponent);
+bootstrap(MainAppComponent, [HTTP_PROVIDERS]);
